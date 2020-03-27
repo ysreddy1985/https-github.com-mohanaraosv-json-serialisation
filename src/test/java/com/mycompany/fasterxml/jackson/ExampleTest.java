5ad0c1da1426809b6c95b26567c8675b5b92d2e8
Mohana Rao S V <mohana.rao.s.v@evry.com>
2020-03-27 17:15:18 +0530

    Another test case for generating json

--- src/test/java/com/mycompany/fasterxml/jackson/ExampleTest.java
+++ src/test/java/com/mycompany/fasterxml/jackson/ExampleTest.java
@@ -4,15 +4,21 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import com.mycompany.fasterxml.jackson.model.Event;
 import com.mycompany.fasterxml.jackson.model.Example;
+import com.mycompany.fasterxml.jackson.model.Metadata;
+import com.mycompany.fasterxml.jackson.model.Source;
 
 public class ExampleTest {
 
@@ -28,5 +34,38 @@ public class ExampleTest {
         String employeeJson = Files.readString(employeePath);
         Example exampleData = objectMapper.readValue(employeeJson, Example.class);
         System.out.println(exampleData);
+    }
+
+    @Test
+    void whenSerializingUsingJsonRootName_thenCorrect() throws JsonProcessingException {
+
+        Source source = new Source();
+        source.setId("1234");
+        source.setReferenceNumber("refereenceId");
+
+        Metadata metadata = new Metadata();
+        metadata.setKey("key");
+        metadata.setValue("SomeValue");
+
+        List<Metadata> metaDatas = new ArrayList<>();
+        metaDatas.add(metadata);
+
+        Event event = new Event();
+        event.setMetadata(metaDatas);
+        event.setType("eventType");
+        List<Event> events = new ArrayList<>();
+        events.add(event);
+
+        Example example = new Example();
+
+        example.setEvents(events);
+        example.setSource(source);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+        String result = objectMapper.writeValueAsString(example);
+
+        System.out.println(result);
     }
 }
